16dfac44eb2cec775fda7eb4aa26706157971309
BadLamb <me@epsilon.pw>
2017-08-17 08:35:01 +0200

    Add WIP support for CDN

--- dexm.js
+++ dexm.js
@@ -2,7 +2,7 @@
     if(navigator.serviceWorker){
         console.log("Starting serviceWorker")
         navigator.serviceWorker.register('serviceWorker.js')
-        //.then(r => window.location.reload())
+        .then(r => window.location.reload())
         .catch(e => alert(e));
     }else{
         var script = document.createElement('script');
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,77 @@
+var BlobBuilder = function () {
+    this.parts = [];
+}
+
+BlobBuilder.prototype.append = function (part) {
+    this.parts.push(part);
+    this.blob = undefined; // Invalidate the blob
+};
+
+BlobBuilder.prototype.getBlob = function () {
+    if (!this.blob) {
+        this.blob = new Blob(this.parts, { type: "text/plain" });
+    }
+    return this.blob;
+};
+
+var sha256=function a(b){function c(a,b){return a>>>b|a<<32-b}for(var d,e,f=Math.pow,g=f(2,32),h="length",i="",j=[],k=8*b[h],l=a.h=a.h||[],m=a.k=a.k||[],n=m[h],o={},p=2;64>n;p++)if(!o[p]){for(d=0;313>d;d+=p)o[d]=p;l[n]=f(p,.5)*g|0,m[n++]=f(p,1/3)*g|0}for(b+="\x80";b[h]%64-56;)b+="\x00";for(d=0;d<b[h];d++){if(e=b.charCodeAt(d),e>>8)return;j[d>>2]|=e<<(3-d)%4*8}for(j[j[h]]=k/g|0,j[j[h]]=k,e=0;e<j[h];){var q=j.slice(e,e+=16),r=l;for(l=l.slice(0,8),d=0;64>d;d++){var s=q[d-15],t=q[d-2],u=l[0],v=l[4],w=l[7]+(c(v,6)^c(v,11)^c(v,25))+(v&l[5]^~v&l[6])+m[d]+(q[d]=16>d?q[d]:q[d-16]+(c(s,7)^c(s,18)^s>>>3)+q[d-7]+(c(t,17)^c(t,19)^t>>>10)|0),x=(c(u,2)^c(u,13)^c(u,22))+(u&l[1]^u&l[2]^l[1]&l[2]);l=[w+x|0].concat(l),l[4]=l[4]+w|0}for(d=0;8>d;d++)l[d]=l[d]+r[d]|0}for(d=0;8>d;d++)for(e=3;e+1;e--){var y=l[d]>>8*e&255;i+=(16>y?0:"")+y.toString(16)}return i};
+
 let peerList = [];
 let ownerPubKey = "";
 let lastTimeStamp = 0;
-const timeout = 60*60
+const timeout = 60 * 60
 
-self.addEventListener('fetch', async function(event){
-    var host = self.location.origin;
+self.addEventListener('fetch', async function (event) {
+    var hostname = self.location.origin;
 
     // Ignore requests not for the hosted origin
-    if(!event.request.url.startsWith(host)){
+    if (!event.request.url.startsWith(hostname)) {
         return;
     }
+
+    // If we haven't feched the peer list in a while do it
+    if (lastTimeStamp < + new Date() - timeout) {
+        //peerList = Object.keys(await (await fetch('http://54.204.137.51:3141/getaddr?json=true')).json());
+    }
+
+    // TODO Fetch contract
+    // Choose random peer
+    //let peer = peerList[Math.floor(Math.random() * peerList.length)];
+
+    var host = 'localhost'
+    var filelen = 10;
+    var proof = {};
+
+    var response = new BlobBuilder();
+
+    for (let i = 0; i < filelen; i++) {
+        var r = await fetch(`http://${host}:8080${self.location.pathname}`,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    block: i,
+                    proof: JSON.stringify(proof),
+                }),
+            })
+
+        
+        let block = await r.text()
+        console.log(block)
+        response.append(block)
+
+        // Generate proof
+
+        let indexes = [0 , 1024 * (i+1)]
+        
+        response.parts.join()
+
+        proof = {
+            indexes: indexes,
+            hash: sha256(response.parts.join().substring(indexes[0], indexes[1])),
+            nonce: 1337,
+        }
+
+    }
+
+    return response.getBlob()
 })
